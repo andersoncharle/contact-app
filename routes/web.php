d8bc2bad479f44c12cb4ad5171a39827b72ab89e
anderson developer <69654069+blackmitnick@users.noreply.github.com>
2022-09-10 10:46:03 +0300

    feat(contact): Define contact create route for controller

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\Routing\Route as RoutingRoute;
 
-Route::get('/contacts',[ContactController::class,'index'])->name('contact.index');
+Route::get('/contacts',[ContactController::class,'index'])->name('contacts.index');
+
+Route::get('/contacts/create',[ContactController::class,'create'])->name('contacts.create');
