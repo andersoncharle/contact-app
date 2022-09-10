edfa1cae08b643ad953c62a5d63d4fc9c1ac0485
anderson developer <69654069+blackmitnick@users.noreply.github.com>
2022-09-10 08:23:36 +0300

    feat(contact): Register contact table seeder to databaseseeder

--- database/seeders/ContactTableSeeder.php
+++ database/seeders/ContactTableSeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
 class ContactTableSeeder extends Seeder
 {
     public function run()
     {
-
+      Contact::factory()->count(50)->create();
     }
 }
