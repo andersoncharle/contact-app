261767e25d2d56dc5c7d7260dd42354ebfce9254
anderson developer <69654069+blackmitnick@users.noreply.github.com>
2022-09-10 11:03:50 +0300

    feat(contact): To public dir add css and js dir for style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,3 +46,10 @@
         </div>
     </div>
 </nav>
+
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
