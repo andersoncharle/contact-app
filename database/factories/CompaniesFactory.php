a9179de1954d89e4e0bedea6793ec8ee210ad619
anderson developer <69654069+blackmitnick@users.noreply.github.com>
2022-09-10 08:33:58 +0300

    feat(copanies): To factories,add Companies factory

---
+++ database/factories/CompaniesFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class CompaniesFactory extends Factory
+{
+    protected $model = Company::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'address' => $this->faker->address(),
+            'website' => $this->faker->word(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
